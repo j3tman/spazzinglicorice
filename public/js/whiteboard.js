7ead8666349dce9cd6d413647f017b6c361ea744
Mitchell Wilcox <mlwilcox@gmail.com>
2015-11-12 17:07:06 -0800

    (Refactor) Prepping angular and board.html for front end merge

--- public/js/whiteboard.js
+++ public/js/whiteboard.js
@@ -1,102 +1,121 @@
-var socket = io();
-
-socket.on('join', function(board) {
-  console.log(board);
-});
-
 //===========global variables===========
-var CANVAS_WIDTH = 800;
-var CANVAS_HEIGHT = 800;
 //===========canvas init===========
+angular.module('whiteboard', ['ui.router'])
+.config(function($stateProvider){
+  $stateProvider
+    .state('eraser', {
+      controller: 'toolbar'
+    });
+})
+.controller('canvas', function($scope, tools){
 
-
-var canvas = $('#whiteboard');
-canvas.width = CANVAS_WIDTH;
-canvas.height = CANVAS_HEIGHT;
-
+  var canvas = $('#whiteboard');
 // if (typeof G_vmlCanvasManager != 'undefined') {
 //   canvas = G_vmlCanvasManager.initElement(canvas);
-context = canvas[0].getContext("2d");
+  context = canvas[0].getContext("2d");
 
-var mouse = {
-  click: false,
-  drag: false,
-  x: 0, 
-  y: 0  
-};
+  var mouse = {
+    click: false,
+    drag: false,
+    x: 0, 
+    y: 0  
+  };
 
-var pen = {
-  fillStyle: 'solid',
-  strokeStyle: "black",
-  lineWidth: 5,
-  lineCap: 'round'
-};
+  //  pen = {
+  //   fillStyle: 'solid',
+  //   strokeStyle: "black",
+  //   lineWidth: 5,
+  //   lineCap: 'round'
+  // };
 
-//=========== socket init functions here=====================
-
-
-//=========== init window/window resize functions here===========
-// var paint = false;
+  //=========== socket init functions here=====================
 
 
+  //=========== init window/window resize functions here===========
+  // var paint = false;
 
-//=========== mouse events============================
-var stroke = [];
-var draw = function(x, y) {
-  context.lineTo(x, y);
-  context.stroke();
-}
-//Mousedown
-canvas.on('mousedown', function(e) {
-//initialize mouse position
-  mouse.click = true;
-  mouse.x = e.pageX - this.offsetLeft;
-  mouse.y = e.pageY - this.offsetTop;
-  //initialize canvas render with current pen style
-  for (key in pen) {
-    context[key] = pen[key];
+
+
+  //=========== mouse events============================
+  var stroke = [];
+  var draw = function(x, y) {
+    context.lineTo(x, y);
+    context.stroke();
   }
+  // Mousedown
+  canvas.on('mousedown', function(e) {
+  //initialize mouse position
+    mouse.click = true;
+    mouse.x = e.pageX - this.offsetLeft;
+    mouse.y = e.pageY - this.offsetTop;
+    //initialize canvas render with current pen style
+    for (key in tools.pen) {
+      context[key] = tools.pen[key];
+    }
 
-  stroke.push([mouse.x, mouse.y]);
+    stroke.push([mouse.x, mouse.y]);
+    //begin draw
+    context.beginPath();
+    context.moveTo(mouse.x, mouse.y);
+  });
 
-  // emit the pen object
-  socket.emit('start', pen);
 
-  //begin draw
-  context.beginPath();
-  context.moveTo(mouse.x, mouse.y);
-});
 
-//drag
-canvas.on('drag', function(e) {
-    mouse.drag = true;
-    var offset = $(this).offset();
-    var x = e.pageX - offset.left;
-    var y = e.pageY - offset.top;
-    draw(x, y);
-    stroke.push([x, y]);
-    console.log([x, y]);
-    
-    //emit x, y in array
-    socket.emit('drag', [x, y]);
+  //drag
+  canvas.on('drag', function(e) {
+      mouse.drag = true;
+      var offset = $(this).offset();
+      var x = e.pageX - offset.left;
+      var y = e.pageY - offset.top;
+      draw(x, y);
+      stroke.push([x, y]);
 
-});
+  //emit data
+      console.log([x, y]);
+  });
 
-canvas.on('dragend', function(e) {
-    mouse.drag = false;
+  canvas.on('dragend', function(e) {
+      mouse.drag = false;
+      mouse.click = false;
+
+   //sample of resulting data to be pushed to db
+      console.log([stroke, tools.pen]);
+      stroke = [];
+   //socket end
+  });
+
+  //do we need this?
+  canvas.onmouseout = function() {
     mouse.click = false;
+    context.closePath();
+  }
+})
 
- //sample of resulting data to be pushed to db
-    console.log([stroke, pen]);
-    stroke = [];
+.controller('toolbar', function($scope, tools) {
+  $scope.changePen = function(option) { 
+    tools.changePen(option);
+  };
+})
 
- //socket end
-  socket.emit('end', null);
-});
+.service('tools', function(){
+  var pen = {
+      fillStyle: 'solid',
+      strokeStyle: "black",
+      lineWidth: 5,
+      lineCap: 'round'
+    };
 
-//do we need this?
-canvas.onmouseout = function() {
-  mouse.click = false;
-  context.closePath();
-}
+    var changePen = function(option) {
+      if (option === 'eraser') {
 
+        this.pen.lineWidth = 50;
+        this.pen.strokeStyle = 'white'
+      } else {
+        this.pen.lineWidth = 5;
+        this.pen.strokeStyle = option;
+      }
+  }
+  return {changePen: changePen, pen: pen}
+
+
+})
